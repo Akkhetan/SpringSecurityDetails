73305c8d4715f75205004c0de6c99b5e741c736b
Ankit Khetan <akkhetan28@gmail.com>
2025-03-20 15:35:30 +0530

    Added the CSRF token to the request through the form.

---
+++ ss_action16/src/main/java/com/ank/ss_action16/SsAction16Application.java
@@ -0,0 +1,13 @@
+package com.ank.ss_action16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsAction16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsAction16Application.class, args);
+	}
+
+}
---
+++ ss_action16/src/test/java/com/ank/ss_action16/SsAction16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ank.ss_action16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsAction16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
